8163c973c3de3ef41162155e45c6cb9454e4321f
Matheus Pessoa <matheus.pessoa16@gmail.com>
2018-11-13 15:58:42 -0300

    secador_graos

--- Arduino/Secador de Grãos/secador_graos.c
+++ Arduino/Secador de Grãos/secador_graos.c
@@ -1,11 +1,11 @@
 /*
-	TO DO
-
-  Curva de secagem não está seguindo o perfil correto
-  Comunicação serial com o bluetooth ou com aplicação desk (funcionando no android via bluetooth)
-  Botão para iniciar o processo de secagem (usar como interrupção externa ou dentro do loop mesmo? Provável que INT)
-
-
+	O sistema possui um botão para iniciar o processo. Quando o sistema está ligado esse
+	botao fica desabilitado.
+	Os sensores são lidos na interrupção e o valor é mostrado em dois LEDs através de um pwm.
+	A curva de secagem é calculada baseada nos sinais vindos dos sensores junto com o tempo.
+	Uso de interrupção por estouro de timer.
+  	Comunicação serial com o bluetooth ou com aplicação desk (funcionando no android via bluetooth)
+  
 */
 
 #include <avr/io.h>
@@ -13,51 +13,59 @@
 #include <avr/pgmspace.h>
 
 // definição das macros
-#define set_bit(y, bit_x) (y |= (1 << bit_x))
-#define clr_bit(y, bit_x) (y &= ~(1 << bit_x))
-#define cpl_bit(y, bit_x) (y ^= (1 << bit_x))
-#define tst_bit(y, bit_x) (y & (1 << bit_x))
+#define set_bit(y, bit_x) (y |= (1 << bit_x)) // colocar o valor do pino em 1
+#define clr_bit(y, bit_x) (y &= ~(1 << bit_x)) // colocar o valor do pino em 0
+#define cpl_bit(y, bit_x) (y ^= (1 << bit_x)) // trocar estado lógico de um pino
+#define tst_bit(y, bit_x) (y & (1 << bit_x)) // lê o valor de um pino
 
-#define LED PB5
-#define conv_ascii 48
-#define tam_vetor 5
+#define LED PB5 // setando LED como o pino 5 do PORTD
+#define conv_ascii 48 // usado para converter em ascii os caracteres recebidos na comunicação serial
+#define tam_vetor 5 // constante com o tamanho para vetores
 
-volatile uint8_t cont = 0, seg = 1;
+// modificador volatile é exigido pelo compilador para
+// variáveis usadas dentro de interrupções.
+volatile uint8_t cont = 0, seg = 1; // variáveis de controle de tempo
 
-volatile uint16_t sensor1, sensor2;
+volatile uint16_t sensor1, sensor2; // variáveis dos sensores. Podemos economizar memória usando inteiros de 16 bits para
+				    // armazenar os valores.
 
+/************ CONFIGURANDO O CANAL AD A SER LIDO ***********/
 
 uint16_t readSensor(uint8_t sensor){
   
-  // configura o AD no canal 0
+  // configura o AD no canal 0 mantendo AVCC como referência
   ADMUX &= 0b01000000;
 
   if(sensor != 0){
-    ADMUX |= 0b00000001;
+    ADMUX |= 0b00000001; // se for diferente do canal zero, define como canal 1
   }
   
-  ADCSRA |= 0b01000000;
-  while (!(ADCSRA & 0b00010000));
+  ADCSRA |= 0b01000000; // inicie a conversão
+  while (!(ADCSRA & 0b00010000)); // espere a conversão ser finalizada (ADIF = 1)
 
   return ADC;
 
 }
+/********************* FINALIZADA A CONFIGURAÇÃO DO AD *********************/
 
 
-ISR(TIMER2_OVF_vect) {
+/********************* CONFIGURANDO A INTERRUPÇÃO *********************/
+// interrupção por estouro do timer2 
+ISR(TIMER2_OVF_vect) { 
   
-  sensor1 = readSensor(0);
-  OCR1A = (sensor1>>2);
-  sensor2 = readSensor(1);
-  OCR1B = (sensor2>>2);
+  sensor1 = readSensor(0); // leia o sensor no canal 0
+  OCR1A = (sensor1>>2); // divide por 4 o valor lido (0 - 1023) para ficar dentro do range do pwm (0 - 255)
+  sensor2 = readSensor(1); // leia o sensor no canal 1
+  OCR1B = (sensor2>>2); // divide por 4 o valor lido (0 - 1023) para ficar dentro do range do pwm (0 - 255)
   
-  cont++;
+  cont++; // a cada 16.384 ms, conta 1 para controlar o tempo.
   if (cont == 61) {
-    seg++;
+    seg++; // 61 estouros do timer é aproximadamente 1 segundo.
     cont = 0;
   }
-
-  if (seg <= 10) {
+	
+  // início do cálculo da curva.
+  if (seg <= 10) { 
     OCR0A = 0;
     OCR0A = (int)(((40 * seg) / (100 * sensor1)) - (-0.4 * sensor2));
   } else {
@@ -84,39 +92,31 @@ ISR(TIMER2_OVF_vect) {
 
 }
 
+/********************* FIM DA INTERRUPÇÃO *********************/
+
+
 int main() {
 
   // Configura o PORTD inteiro como saída
-  DDRD = 0xFF;
+  DDRD = 0b11111111;
   
   // Configura os pinos 1, 2 e 3 do PORTB como saídas
   DDRB |= 0b00001110;
   PORTB |= 0xFF; // alta impedância nos pinos do PORTB que são entradas e nível alto nos pinos que estão como saída
 
   // Configurando o conversor A/D
-  ADMUX = 0b01000000;
-  ADCSRA = 0b10000111; // configurando para a frequência mais baixa para usar os 10 bits de precisão
-
+  ADMUX = 0b01000000; // referência de tensão: AVCC
+  ADCSRA = 0b10000111; // configurando para a frequência mais baixa para usar os 10 bits de precisão e habilitando o adc.
 
   // Configurando PWM usando o TIMER0
   TCCR0A = 0b10100011; //PWM não invertido nos pinos OC0A e OC0B
   TCCR0B = 0b00000011; //liga TC0, prescaler = 64
   OCR0A = 0;    //controle do ciclo ativo do PWM 0C0A
-  //    OCR0B = 50;    //controle do ciclo ativo do PWM OC0B
-  //    TCCR0A = 0b01100011; //TOP = OCR0A, OC0A e OC0B habilitados
-  //    TCCR0B = 0b00001001; //liga TC0, prescaler = 1 e ajusta modo para comparação com OCR0A
-  //    OCR0A = 100;    //controle da período do sinal no pino OC0A
-  //    OCR0B = 10;    //controle do ciclo ativo do PWM 0C0B
-
 
   // CONFIGURAÇÃO DO PWM TIMER1
   TCCR1A = 0b10100010;    //PWM não invertido nos pinos OC1A e OC1B
   TCCR1B = 0b00011001;    //liga TC1, prescaler = 1
   ICR1 = 255;    //valor máximo para contagem
-  //OCR1A = 2000;    //controle do ciclo ativo do PWM 0C1A
-  //OCR1B = 100;
-
-
 
   // CONFIGURANDO A INTERRUPÇÃO DO TIMER2
   cli(); // desliga interrupções globais
@@ -124,25 +124,16 @@ int main() {
   TIMSK2 = 0b00000001; // interrupção do TC2 habilitada
   sei(); // ativa as interrupções globais
   
-  
   // Inicializa a comunicação serial
   initUSART();
 
-  // vetor que contém os valores da serial convertidos
+  // vetor que contém os valores lidos da serial convertidos para ascii
   uint8_t digitos[tam_vetor];
-
-  //uint8_t palavra[10];
-
-  
-  escreve_USART("Valores Sensor1 \n");
-  //uint8_t cont=0;
   
   while (1) {
-    // put your main code here, to run repeatedly:
-
+	  
     // primeiro convertemos número a número os valores recebidos 
     // exemplo: recebi 255, separa em 2 5 e 5 e os envia para a serial
-    
     ident_num((unsigned int) sensor1, digitos);
     txByte(digitos[3]);
     txByte(digitos[2]);
@@ -166,27 +157,11 @@ int main() {
     txByte(digitos[0]);
     //digitos[4] = ';';
     txByte(digitos[4]);
+	  
+// por alguma razão o app android não está recebendo o último dígito. Assim
+// estou enviando novamente o digito[0]. Esse é um bug a ser consertado.	
 
-    
-    
-//  	// por alguma razão o app android não está recebendo o último dígito. Assim
-//  	// estou enviando novamente o digito[0]. Esse é um bug a ser consertado.	
-  	//txByte(digitos[0]);    
-//    //txByte('\t');
-    txByte('\n');
-
-//    while(rxByte() != '\n'){
-//      palavra[cont] += rxByte();
-//      cont++;
-//    }
-//
-//    if(palavra == "ldr"){
-//      set_bit(PORTB, LED);
-//    }else{
-//      clr_bit(PORTB, LED);
-//    }
-//    cont=0;
-    
+    txByte('\n'); // final da comunicação
   }
 
 }
@@ -201,13 +176,13 @@ void initUSART()
   //UCSRA = (1<<U2X);
 
   // UCSZ2 = 0 - 8 bits
-  UCSR0B |= _BV(RXEN0) | _BV(TXEN0);
+  UCSR0B |= _BV(RXEN0) | _BV(TXEN0); // habilita o transmissor e o receptor serial
 
 
   // UCSZ1 = 1 e UCSZ0 = 1 -> 8 bits
   // USBS0 = 0 -> 1 - stop bits
   // UPM0 = 0 e UMP0 = 0 -> sem bit de paridade
-  UCSR0C |= _BV(UCSZ01) | _BV(UCSZ00);
+  UCSR0C |= _BV(UCSZ01) | _BV(UCSZ00); // frame de 8 bits
 }
 
 void txByte(uint8_t info)
@@ -236,7 +211,7 @@ uint8_t transceiver(uint8_t *data){
 
 //Conversão de um número em seus digitos individuais.
 //O papel dessa função é poder exibir corretamente os valores na porta serial, já
-// que algun serão maiores que 255 e alguns bugs estranhos acontecem mesmo com valores
+// que alguns serão maiores que 255 e alguns bugs estranhos acontecem mesmo com valores
 // menores;
 //------------------------------------------------------------------------------------
 void ident_num(unsigned int valor, unsigned char *disp)
@@ -249,7 +224,7 @@ void ident_num(unsigned int valor, unsigned char *disp)
     *disp = (valor % 10) + conv_ascii;
     valor /= 10;
     disp++;
-    //limpa vetor para armazenagem dos digitos
+    //limpa vetor para armazenamento dos digitos
     //pega o resto da divisão por 10
     //pega o inteiro da divisão por 10
   } while (valor != 0);
@@ -269,4 +244,3 @@ void escreve_USART(unsigned char c[], int t){
     txByte(c[i]);
   }  
 }
-
